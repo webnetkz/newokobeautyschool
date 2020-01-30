c204ee266253fd61b43e4cad8a60eb8fb3276959
webnetkz <8gg001@gmail.com>
2020-01-30 13:28:00 +0600

    start project

---
+++ public/tamplates/header.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="theme-color" content="rgb(88, 139, 221)">
+    <meta name="robots" content="index, follow">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="description" content="<?=$description?>">
+    <meta name="keywords" content="<?=$keywords?>">
+    <meta name="author" content="TOO WebNet">
+
+    <!-- CODELAB: Add iOS meta tags and icons -->
+    <meta name="apple-mobile-web-app-capable" content="yes">
+    <meta name="apple-mobile-web-app-status-bar-style" content="black">
+    <meta name="apple-mobile-web-app-title" content="newProduct">
+    <link rel="apple-touch-icon" href="/images/logo.png">
+    <link rel="stylesheet" href="public/styles/allStyle.css">
+    <link rel="stylesheet" href="public/styles/buttons.css">
+    <link rel="manifest" href="manifest.json">
+    <link rel="shortcut icon" href="public/images/mini_logo.png" type="image/png">
+
+    <title><?=$title?></title>
+
+    <script src="public/scripts/main.js"></script>
+</head>
+<body>
+    <button class="btnMenu" onclick="handlerMenu();">Меню</button>
+    <header class="headerMenu">
+        <menu class="hiddenMenu">
+            <nav>
+
+            </nav>
+        </menu>
+
+    </header>
